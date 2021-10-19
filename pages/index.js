825bb1af66e8abf2cfa57be0c8125f10ed4c18c5
trentnixon <trentnixon@gmail.com>
2021-10-19 14:29:54 +1100

    heroku test

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,8 @@ import { API,server } from "../config/index"
 /*
 git add .
 git commit -am " Int Strapi Test"
-
 git push heroku main:main
-git push heroku master
+
 */
 export default function Home({mission}) {
   //console.log(articles)
