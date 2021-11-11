693fdb6041b5225ecd1e49035c7a2c533184e698
trentnixon <trentnixon@gmail.com>
2021-11-11 22:49:37 +1100

    remove location

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000/' : 'https://sjwca.herokuapp.com/'
+export const server = dev ? 'http://localhost:3000/' : 'http://localhost:3000/'
+//'https://sjwca.herokuapp.com/'
 export const API = dev ? 'http://localhost:1337/' : 'https://sjwca-strapi.herokuapp.com/'
