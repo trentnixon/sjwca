bd3673d7f26cb2d9cb30fc8c1a89bf352bc3fea2
trentnixon <trentnixon@gmail.com>
2022-02-15 21:25:28 +1100

    index api test

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000/' : 'https://www.sydneyjuniorwintercricket.org.au/'
+
+export const server = dev ? '/' : '/'
 export const API = dev ? 'http://localhost:1337/' : 'https://sjwca-strapi.herokuapp.com/';
+ 
