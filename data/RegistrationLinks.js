d20fd6d3883eb4c96b45a3f8e7d095e252d3d326
trentnixon <trentnixon@gmail.com>
2024-02-19 11:46:44 +1100

    Push New Version

--- data/RegistrationLinks.js
+++ data/RegistrationLinks.js
@@ -3,43 +3,43 @@ export const Links = {
     InnerWestCanterburyBankstown: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/2fb102",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     },
     EasternSuburbs: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/2f5529",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     },
    /*  NorthernSuburbs: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/2f5529",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     }, */
     SouthWestSydney: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/ef6ba6",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     },
     Parramatta: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/f69e3e",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     },
     WesternSydneyRegion: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/f69e3e",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     },
     CentralCoast: {
       Name: "Register your Team",
       Blurb: "",
-      Href: "https://www.playhq.com/cricket-australia/register/a65b3e",
+      Href: "https://www.playhq.com/cricket-australia/register/97e910",
     },
     
   },
   EOF: {
     Name: "Register a Player",
     Blurb: "Something about the Registration process",
-    Href: "https://www.playhq.com/cricket-australia/register/cd986e",
+    Href: "https://www.playhq.com/cricket-australia/register/c3064c",
   },
 };
