2aa7f0952fbd28831eacd13e0fdba668f3dfd0bd
trentnixon <trentnixon@gmail.com>
2024-02-26 14:38:29 +1100

    Push New Version

--- components/RegistrationLandingPage/index.js
+++ components/RegistrationLandingPage/index.js
@@ -9,11 +9,11 @@ const RegistrationLandingPage = (props) => {
   return (
     <>
       <RegistrationHeader {...props} />
-      <Section1 />
+     {/*  <Section1 />
       <Section2 />
       <Section3 />
       <Section4 />
-      <Section5 />
+      <Section5 /> */}
     </>
   );
 };
